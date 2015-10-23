565de504e691ed5e4d5b80febd2586900b917157
Dan Leslie <dan@ironoxide.ca>
2015-10-22 22:55:36 -0700

    Some simplification

--- nanovg.scm
+++ nanovg.scm
@@ -16,16 +16,16 @@
 
 (define-foreign-record-type (context "NVGcontext"))
 
-(define create-context
+(define create-context*
   (cond-expand
-    (nanovg-gl2 (foreign-lambda* context ((int flags)) "C_return(nvgCreateGL2(flags));"))
-    (nanovg-gl3 (foreign-lambda* context ((int flags)) "C_return(nvgCreateGL3(flags));"))
-    (nanovg-gles2 (foreign-lambda* context ((int flags)) "C_return(nvgCreateGLES2(flags));"))
-    (nanovg-gles3 (foreign-lambda* context ((int flags)) "C_return(nvgCreateGLES3(flags));"))))
+    (nanovg-gl2 (foreign-lambda context "nvgCreateGL2" int))
+    (nanovg-gl3 (foreign-lambda context "nvgCreateGL3" int))
+    (nanovg-gles2 (foreign-lambda context "nvgCreateGLES2" int))
+    (nanovg-gles3 (foreign-lambda context "nvgCreateGLES3" int))))
 
-(define delete-context
+(define delete-context!
   (cond-expand
-    (nanovg-gl2 (foreign-lambda* void ((context ctx)) "nvgDeleteGL2(ctx);"))
-    (nanovg-gl3 (foreign-lambda* void ((context ctx)) "nvgDeleteGL3(ctx);"))
-    (nanovg-gles2 (foreign-lambda* void ((context ctx)) "nvgDeleteGLES2(ctx);"))
-    (nanovg-gles3 (foreign-lambda* void ((context ctx)) "nvgDeleteGLES3(ctx);"))))
+    (nanovg-gl2 (foreign-lambda void "nvgDeleteGL2" context))
+    (nanovg-gl3 (foreign-lambda void "nvgDeleteGL3" context))
+    (nanovg-gles2 (foreign-lambda void "nvgDeleteGLES2" context))
+    (nanovg-gles3 (foreign-lambda void "nvgDeleteGLES3" context))))
